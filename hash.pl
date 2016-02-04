aed12a6ca81fcea35bbc9f2bd3887d91690ec14b
szhukov81 <szhukov81@gmail.com>
2016-02-04 12:56:46 +0200

    Added example with each function

--- hash.pl
+++ hash.pl
@@ -3,6 +3,14 @@
 use strict;
 
 #Sorting hash by values and keys
+print "\nSorting by 2 values\n\n";
+
 my %hash1 = qw(a 12 b 10 c 23 d 12  e 10 f 15 g 23 h 23); 
 
 print "$hash1{$_} - $_  \n" for sort { $hash1{$b} <=> $hash1{$a} || $a cmp $b} keys %hash1;
+
+#Reading all values
+print "\nRead all values from hash\n\n";
+while ( my ($key, $value) = each %hash1 ) {
+    print "$key - $value\n";
+}
